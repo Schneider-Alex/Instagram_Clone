5467e1932ef7603868b9457eca26a795532c551e
Schneider-Alex <alexmanuelschneider@gmail.com>
2022-06-22 12:46:47 -0400

    logo switches appropriately

---
+++ instagram_clone/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Image from "next/image";
+
+function Header() {
+    return (
+        <div>
+            <div className="flex justify-between bg-white max-w-6xl">
+                <div className="relative hidden lg:inline-grid h-24 w-24">
+                    <Image src="https://links.papareact.com/ocw" layout="fill" objectFit="contain"/>
+                </div>
+                <div className="relative lg:hidden h-10 w-10 flex-shrink-0">
+                    <Image src="https://links.papareact.com/jjm" layout="fill" objectFit="contain"/>
+                </div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    )
+}
+
+export default Header
