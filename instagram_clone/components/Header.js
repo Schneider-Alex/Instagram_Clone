b10d1f2a99eb72ff8b38fdb0443fbf373e69f144
Schneider-Alex <alexmanuelschneider@gmail.com>
2022-06-22 13:03:08 -0400

    install tailwind css forms
    method of overriding default form styling easily from https://github.com/tailwindlabs/tailwindcss-forms

--- instagram_clone/components/Header.js
+++ instagram_clone/components/Header.js
@@ -1,17 +1,30 @@
 import React from 'react'
 import Image from "next/image";
+import {
+    SearchIcon,
+    PlusCircleIcon,
+    UserGroupIcon,
+    HeartIcon,
+    PaperAirplaneIcon,
+    MenuIcon
+} from "@heroicons/react/outline";
 
 function Header() {
     return (
         <div>
             <div className="flex justify-between bg-white max-w-6xl">
-                <div className="relative hidden lg:inline-grid h-24 w-24">
-                    <Image src="https://links.papareact.com/ocw" layout="fill" objectFit="contain"/>
+                <div className="relative hidden lg:inline-grid w-24">
+                    <Image src="https://links.papareact.com/ocw" layout="fill" objectFit="contain" />
                 </div>
-                <div className="relative lg:hidden h-10 w-10 flex-shrink-0">
-                    <Image src="https://links.papareact.com/jjm" layout="fill" objectFit="contain"/>
+                <div className="relative lg:hidden  w-10 flex-shrink-0">
+                    <Image src="https://links.papareact.com/jjm" layout="fill" objectFit="contain" />
                 </div>
-                <div></div>
+                <div className="relative mt-1 p-3 rounded-md">
+                    <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
+                        <SearchIcon className="h-5 w-5 text-gray-500"/>
+                    </div>
+                    <input className=" bg-gray-50 block w-full pl-10 sm:text-sm border-gray-300 focus:ring-black focus:border-black" type="text" placeholder="Search"/>
+                </div>
                 <div></div>
             </div>
         </div>
