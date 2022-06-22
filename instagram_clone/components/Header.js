8216aef4a1666d9f75033f5fdeaa17b53dd5293f
Schneider-Alex <alexmanuelschneider@gmail.com>
2022-06-22 13:08:23 -0400

    Finished search and right image

--- instagram_clone/components/Header.js
+++ instagram_clone/components/Header.js
@@ -23,7 +23,7 @@ function Header() {
                     <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
                         <SearchIcon className="h-5 w-5 text-gray-500"/>
                     </div>
-                    <input className=" bg-gray-50 block w-full pl-10 sm:text-sm border-gray-300 focus:ring-black focus:border-black" type="text" placeholder="Search"/>
+                    <input className=" bg-gray-50 block w-full pl-10 sm:text-sm border-gray-300 focus:ring-black focus:border-black rounded-md" type="text" placeholder="Search"/>
                 </div>
                 <div></div>
             </div>
