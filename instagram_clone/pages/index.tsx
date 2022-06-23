d684d403bd6fabfd8c0f3395f77be71d698271ed
Schneider-Alex <alexmanuelschneider@gmail.com>
2022-06-23 13:55:08 -0400

    STUCK on faker.helpers.contextualCard()

--- instagram_clone/pages/index.tsx
+++ instagram_clone/pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Header from "../components/Header";
+import Feed from "../components/Feed"
 
 const Home: NextPage = () => {
   return (
@@ -15,6 +16,7 @@ const Home: NextPage = () => {
       <Modal></Modal> */}
 
       <Header/>
+      <Feed/>
     </div>
   )
 }
