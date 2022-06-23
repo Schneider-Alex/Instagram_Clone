a9962ddcf210f6d7e210d28b241df805d0b00ece
Schneider-Alex <alexmanuelschneider@gmail.com>
2022-06-23 12:37:14 -0400

    custom NavBtn Class built and applied to nav icons

--- instagram_clone/components/Header.js
+++ instagram_clone/components/Header.js
@@ -31,10 +31,10 @@ function Header() {
                     </div>
                 </div>
                 <div className="flex items-center justify-end space-x-4">
-                    <HomeIcon className="h-10 w-10" />
+                    <HomeIcon className="navBtn" />
                     <MenuIcon className="h-6 md:hidden
                     cursor-pointer " />
-                    <PaperAirplaneIcon className="h-10 w-10"/>
+                    <PaperAirplaneIcon className="navBtn"/>
 
                 </div>
 
